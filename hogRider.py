735c587a7d0a701cce588b69a3f7e509248a6330
Adam Brickell <adam-b130@hotmail.com>
2023-01-21 15:27:55 -0800

    tested concept

--- hogRider.py
+++ hogRider.py
@@ -8,10 +8,21 @@ def findLocations():
 
     while(cont):
         try:
-            print(EnumKey(rawKey,index))
+            addr = EnumKey(rawKey,index)
+            rawKey2 = OpenKey(registry, "AppEvents\Schemes\Apps\.Default" + '\\' + addr)
+            addr2 = EnumKey(rawKey2,0)
+            rawKey3 = OpenKey(registry, "AppEvents\Schemes\Apps\.Default" + '\\' + addr + '\\' + addr2)
+            try:
+                print("AppEvents\Schemes\Apps\.Default" + '\\' + addr + '\\' + addr2)
+                print(EnumValue(rawKey3,0))
+            except:
+                print("error")
             index += 1
         except:
             cont = False
-        
+            print("sdkjhf")
+
+        print("\n")
 
 findLocations()
+
